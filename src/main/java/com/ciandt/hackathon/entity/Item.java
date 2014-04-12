f7837f892e2e21e41323a7361219ebca796beaea
Adriano de Palma <adrianodepalma@gmail.com>
2014-04-12 12:20:28 -0300

    Item entity

---
+++ src/main/java/com/ciandt/hackathon/entity/Item.java
@@ -0,0 +1,52 @@
+package com.ciandt.hackathon.entity;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class Item {
+
+	@Id
+	private int id;
+	private String name;
+	private int points;
+	private double value;
+
+	public Item() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	
+}
