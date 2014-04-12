68673e3cbec4fcd3967078718e8ecc29d81861ff
rrocha <rrocha@cit010875.cit>
2014-04-12 11:37:10 -0300

    Info

--- src/main/java/com/ciandt/hackathon/resources/PurchaseServlet.java
+++ src/main/java/com/ciandt/hackathon/resources/PurchaseServlet.java
@@ -3,6 +3,7 @@ package com.ciandt.hackathon.resources;
 import java.io.IOException;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,19 +17,36 @@ import com.google.inject.Singleton;
 @SuppressWarnings("serial")
 @Singleton
 public class PurchaseServlet extends HttpServlet {
+
+		
+		
+	@PostConstruct
+	private void setup() {
+		mockProducts();
+	}
 	
 	@Inject
 	private ProductDAO productDao;
-	
-	
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
-		
-		
-		//read products
-	    List<Product> products = productDao.findAllProducts();
-		req.setAttribute( "products", products );	
-		
+		// read products
+		List<Product> products = productDao.findAllProducts();
+		req.setAttribute("products", products);
+		System.out.println("Products Size:" + products.size());
+
 		req.getRequestDispatcher("/purchase.jsp").forward(req, resp);
+	}
+
+	private void mockProducts() {
+		Product product = new Product();
+
+		product.setDescription("Balao 1");
+		product.setImageURL("http://uol.com.br/image/gif");
+		product.setPrice(10.0);
+		product.setPoints(0);
+		product.setType("A");
+
+		productDao.insert(product);
 	}
 }
