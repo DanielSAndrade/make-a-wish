3770f223d5a1d08bfb31d4caaefd7451d4f34830
Roger Maedo <rogermaedo@hotmail.com>
2014-04-12 15:24:49 -0300

    Correcao pagina detalhe

--- src/main/java/com/ciandt/hackathon/resources/DetalhesProdutoServlet.java
+++ src/main/java/com/ciandt/hackathon/resources/DetalhesProdutoServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ciandt.hackathon.dao.ProdutoDAO;
+import com.ciandt.hackathon.entity.Produto;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -21,6 +22,11 @@ public class DetalhesProdutoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
 		String idProduto = req.getParameter("idProduto");
-		produtoDAO.findById(idProduto);
+		Produto produto = produtoDAO.findById(idProduto);
+		
+        req.setAttribute("produto", produto);
+		
+        req.getRequestDispatcher("/produto/detalhes").forward(req, resp);		
+		
 	}
 }
