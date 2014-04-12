b0e6eab3f77c10d2466f11b9142922efc42826b6
Douglas Drumond <drumond.douglas@gmail.com>
2014-04-12 13:21:24 -0300

    Mock da tela inicial
    Signed-off-by: Douglas Drumond <drumond.douglas@gmail.com>

---
+++ src/main/webapp/static/js/main.js
@@ -0,0 +1,11 @@
+(function($){
+
+$('#btnFilme1, #btnFilme2').click(function(){
+Voto.votar($('#formVotos'), this, $('#filmeVotado'));
+});
+
+$('#btnSalvarUsuario').click(function(){
+Usuario.atualizar();
+});
+
+})(jQuery);
