6dee783978dc38cc07ef24a2da0caaae0e9e5564
brunnoattorre <brattorre@gmail.com>
2014-04-12 14:00:15 -0300

    Commit Services

--- src/main/java/com/ciandt/hackathon/api/CommonResource.java
+++ src/main/java/com/ciandt/hackathon/api/CommonResource.java
@@ -11,8 +11,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import com.ciandt.hackathon.business.ContabilPontuacao;
+import com.ciandt.hackathon.business.Visualizador;
 import com.ciandt.hackathon.dao.GreetingDAO;
+import com.ciandt.hackathon.entity.Compra;
 import com.ciandt.hackathon.entity.Item;
+import com.ciandt.hackathon.entity.Pontuacao;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -25,6 +29,12 @@ public class CommonResource {
 	private final GreetingDAO greetingDAO;
 
 	@Inject
+	public Visualizador visualizador;
+
+	@Inject
+	public ContabilPontuacao contabilPontuacao;
+
+	@Inject
 	public CommonResource(GreetingDAO greetingDAO) {
 		super();
 		this.greetingDAO = greetingDAO;
@@ -35,14 +45,43 @@ public class CommonResource {
 	public List<Item> listGreetings(@Context HttpServletRequest request) {
 		List<Item> listItens = new ArrayList<Item>();
 
-		listItens.add(this.createMockItem(1L, "Bicicleta", new Double(200), "Descrição da bicicleta", "/bicicleta.jpg"));
-		listItens.add(this.createMockItem(2L, "PlayStation", new Double(4000), "Descrição do playstation", "/playstation.jpg"));
-		listItens.add(this.createMockItem(3L, "Disney", new Double(3000), "Descrição da Disney", "/disney.jpg"));
+		listItens.add(this.createMockItem(1L, "Bicicleta", new Double(200),
+				"Descrição da bicicleta", "/bicicleta.jpg"));
+		listItens.add(this.createMockItem(2L, "PlayStation", new Double(4000),
+				"Descrição do playstation", "/playstation.jpg"));
+		listItens.add(this.createMockItem(3L, "Disney", new Double(3000),
+				"Descrição da Disney", "/disney.jpg"));
 
 		return listItens;
 	}
 
-	private Item createMockItem(Long id, String nome, Double preco, String descricao, String path) {
+	@GET
+	@Path("/listItens")
+	public List<Item> listItens(@Context HttpServletRequest request) {
+		List<Item> listItens = new ArrayList<Item>();
+		listItens = visualizador.findItens();
+		return listItens;
+	}
+
+	@GET
+	@Path("/listPontuacao")
+	public List<Pontuacao> listPontuacoes(@Context HttpServletRequest request) {
+		List<Pontuacao> listPontuacaos = new ArrayList<Pontuacao>();
+		listPontuacaos = visualizador.findPontuacoes();
+		return listPontuacaos;
+	}
+
+	@GET
+	@Path("/comprarItem")
+	public void efetuaCompra(@Context HttpServletRequest request) {
+
+		Long idPessoa = 0l;
+		Long idItem = 0l;
+		contabilPontuacao.criaCompra(idItem, idPessoa);
+	}
+
+	private Item createMockItem(Long id, String nome, Double preco,
+			String descricao, String path) {
 		Item item = new Item();
 		item.setId(id);
 		item.setNome(nome);
