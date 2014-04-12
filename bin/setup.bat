4c7efb3a96c49a39fd94c439ca4283b9abced24b
Alexandre Junqueira <alexjunq@gmail.com>
2014-04-12 13:22:02 -0300

    revisao local

---
+++ bin/setup.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=C:\Tools\Java\jdk1.7.0_45
+set M2_HOME=C:\Tools\apache-maven-3.1.1
+set M2=%M2_HOME%\bin
+set MAVEN_OPTS=-Xms256m -Xmx1024m
+
+set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
