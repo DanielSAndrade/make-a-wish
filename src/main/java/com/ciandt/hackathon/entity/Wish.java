02d7522bc7c5b368557000e9c849130e2df14ac7
Adriano de Palma <adrianodepalma@gmail.com>
2014-04-12 11:36:01 -0300

    Wish Entity

--- src/main/java/com/ciandt/hackathon/entity/Wish.java
+++ src/main/java/com/ciandt/hackathon/entity/Wish.java
@@ -1,7 +1,78 @@
 package com.ciandt.hackathon.entity;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+ 
+@Entity
 public class Wish {
 	
+	@Id
+	private int id;
+	private String childName;
+	private int childAge;
+	private String disease;
+	private String wish;
+	private int status;
+	private int donator;
 	
+	public Wish() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getChildName() {
+		return childName;
+	}
+
+	public void setChildName(String childName) {
+		this.childName = childName;
+	}
+
+	public int getChildAge() {
+		return childAge;
+	}
+
+	public void setChildAge(int childAge) {
+		this.childAge = childAge;
+	}
+
+	public String getDisease() {
+		return disease;
+	}
+
+	public void setDisease(String disease) {
+		this.disease = disease;
+	}
+
+	public String getWish() {
+		return wish;
+	}
+
+	public void setWish(String wish) {
+		this.wish = wish;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getDonator() {
+		return donator;
+	}
+
+	public void setDonator(int donator) {
+		this.donator = donator;
+	}
  
 }
