45d4154c951a2cfdbf0c204a7df7a731bd8ee70d
marcosvidolin <marcosvidolin@gmail.com>
2014-04-12 11:59:24 -0300

    Venda resource

--- src/main/java/com/ciandt/hackathon/api/VendaResource.java
+++ src/main/java/com/ciandt/hackathon/api/VendaResource.java
@@ -1,14 +1,19 @@
 package com.ciandt.hackathon.api;
 
+import java.util.Date;
+
 import javax.annotation.concurrent.ThreadSafe;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import com.ciandt.hackathon.dao.ProdutoDAO;
 import com.ciandt.hackathon.dao.VendaDAO;
+import com.ciandt.hackathon.entity.Produto;
 import com.ciandt.hackathon.entity.Venda;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -20,24 +25,29 @@ import com.google.inject.Singleton;
 public class VendaResource {
 
 	private VendaDAO vendaDAO;
-	
+
+	@Inject
+	private ProdutoDAO produtoDAO;
+
 	@Inject
 	public VendaResource(VendaDAO vendaDAO) {
 		super();
 		this.vendaDAO = vendaDAO;
 	}
-	
+
 	@POST
 	@Path("/insertVenda")
-	public Venda insertVenda(@Context HttpServletRequest request) {
-		//id produto
-		//numero Mesa
-		//id comprador
-		
-		return null;
-		
-//		Venda venda = new Venda();
-		
+	public Venda insertVenda(@Context HttpServletRequest request,
+			@FormParam("idProd") Long idProd,
+			@FormParam("precoProd") Double precoProd,
+			@FormParam("tipoProd") String tipoProd,
+			@FormParam("numMesa") Long numMesa,
+			@FormParam("codComp") Long codComp) {
+
+		Produto produto = produtoDAO.findById(idProd);
+
+		Venda venda = new Venda(produto, numMesa, new Date(), codComp);
+		vendaDAO.insert(venda);
+		return venda;
 	}
-	
 }
