1910a863d31c1f1dc2584149c89a64055e529462
kanjih <ksolucoesinteligentes@gmail.com>
2014-04-12 14:48:16 -0300

    [kanjih] mudei os produtos

--- src/main/java/com/ciandt/hackathon/services/impl/ProductServiceImpl.java
+++ src/main/java/com/ciandt/hackathon/services/impl/ProductServiceImpl.java
@@ -8,40 +8,41 @@ import com.ciandt.hackathon.entity.Product;
 import com.ciandt.hackathon.services.ProductService;
 import com.google.inject.Inject;
 
-
 public class ProductServiceImpl implements ProductService {
-	
+
+	private String[] productName = { "Conhecer Parque Harry Porter",
+			"Guitarra", "PS3", "PS4","Guitarra", "SmartPhone", "NoteBook pelo Dentinho", "Disney", "Comer um peixe beira mar", "Trabalhar na Ci&T" };
+
 	@Inject
 	private ProductDAO productDAO;
 
 	@Override
 	public List<Product> listProducts() {
-		
-		List<Product> listProducts =  productDAO.findProduct();	
-		
-		if(listProducts == null || listProducts.size() == 0){
+
+		List<Product> listProducts = productDAO.findProduct();
+
+		if (listProducts == null || listProducts.size() == 0) {
 			listProducts = this.loadProduct();
 		}
-		
-		
+
 		return listProducts;
 	}
-	
+
 	/**
 	 * 
 	 */
 	private List<Product> loadProduct() {
-		List<Product> listProducts =  new ArrayList<Product>();
-		for (int i = 0; i <= 100; i++) {
+		List<Product> listProducts = new ArrayList<Product>();
+		for (int i = 0; i < 10; i++) {
 			Product product = new Product();
-			product.setDescription("product product product product product productproduct product productproduct product product" + i);
+			product.setDescription("Este produto faz uma criança feliz " + productName[i]);
 			product.setId((long) i);
-			product.setName("product");
+			product.setName(productName[i]);
 			product.setValue((long) (Math.random() * 1000));
 			product.setWeight((long) (Math.random() * 10));
 			listProducts.add(product);
 		}
-		
+
 		return listProducts;
 	}
 
