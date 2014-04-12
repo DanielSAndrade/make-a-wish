69a131c9579d1e18c6b6bc942b7456d7c8ba11eb
marcos <markinster@gmail.com>
2014-04-12 14:20:01 -0300

    getters and setters Table

--- src/main/java/com/ciandt/hackathon/entity/Table.java
+++ src/main/java/com/ciandt/hackathon/entity/Table.java
@@ -8,12 +8,42 @@ import com.googlecode.objectify.annotation.Id;
 @Entity
 public class Table {
 
-
 	@Id
 	private String name;
 	private String description;
 	private Long score;
 	private List<User> users;
-	
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getScore() {
+		return score;
+	}
+
+	public void setScore(Long score) {
+		this.score = score;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
 }
