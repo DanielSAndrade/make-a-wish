14291e3e583ffe2bb262e8e239b9d9f2dc86a253
Adriano de Palma <adrianodepalma@gmail.com>
2014-04-12 12:13:53 -0300

    Table entity

---
+++ src/main/java/com/ciandt/hackathon/entity/Table.java
@@ -0,0 +1,52 @@
+package com.ciandt.hackathon.entity;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class Table {
+
+	@Id
+	private int id;
+	private String name;
+	private int points;
+	private int badges;
+
+	public Table() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public int getBadges() {
+		return badges;
+	}
+
+	public void setBadges(int badges) {
+		this.badges = badges;
+	}
+	
+	
+}
