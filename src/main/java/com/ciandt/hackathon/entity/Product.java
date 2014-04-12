de22bbc2b92e72acbb53de64c24ea4da0bbe872a
Rafael Camargo <rafael.camargo.sp@gmail.com>
2014-04-12 14:09:53 -0300

    ....

--- src/main/java/com/ciandt/hackathon/entity/Product.java
+++ src/main/java/com/ciandt/hackathon/entity/Product.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ciandt.hackathon.entity;
 
 import com.googlecode.objectify.annotation.Entity;
@@ -47,58 +46,3 @@ public class Product {
 	}
 
 }
-=======
-
-package com.ciandt.hackathon.entity;
-
-import com.googlecode.objectify.annotation.Entity;
-import com.googlecode.objectify.annotation.Id;
-
-@Entity
-public class Product {
-
-	@Id
-	private Long id;
-	private String imageURL;
-	private String description;
-	private Double price;
-	private String type;
-	private Integer points;
-	
-    public Long getId() {
-        return id;
-    }
-    
-	public String getImageURL() {
-		return imageURL;
-	}
-	public void setImageURL(String imageURL) {
-		this.imageURL = imageURL;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Double getPrice() {
-		return price;
-	}
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public Integer getPoints() {
-		return points;
-	}
-	public void setPoints(Integer points) {
-		this.points = points;
-	}
-
-}
->>>>>>> branch 'fernando_noronha' of https://github.com/fapereira1/make-a-wish.git
