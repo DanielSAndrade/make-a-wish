2bc01a2416e2d91f2ebfb0f0ed58528d6af200a7
Roger Maedo <rogermaedo@hotmail.com>
2014-04-12 10:43:46 -0300

    Iniciacao Mesa DAO

---
+++ src/main/java/com/ciandt/hackathon/dao/MesaDAO.java
@@ -0,0 +1,17 @@
+package com.ciandt.hackathon.dao;
+
+import java.util.List;
+
+import com.ciandt.hackathon.entity.Mesa;
+
+public class MesaDAO {
+
+	public List<Mesa> getListagemDAO() {
+		
+		Mesa mesa = new Mesa();
+		
+		return null;
+		
+	}
+	
+}
