090fc517298590c66ab8b73dd37a010d0a39e76c
Bruno <brungcm@gmail.com>
2014-04-12 13:07:24 -0300

    ajuste

---
+++ src/main/java/com/ciandt/hackathon/resources/Poder.java
@@ -0,0 +1,13 @@
+package com.ciandt.hackathon.resources;
+
+public class Poder {
+	
+	public static final String nivel1 = "Aprendiz";
+	
+	public static final String nivel2 = "Iniciante";
+	
+	public static final String nivel3 = "Profissional";
+	
+	public static final String nivel4 = "Mestre dos Mágicos";
+
+}
---
+++ src/main/java/com/ciandt/hackathon/resources/TopTop.java
@@ -0,0 +1,73 @@
+package com.ciandt.hackathon.resources;
+
+public class TopTop {
+
+	private String name;
+
+	private Double valor;
+
+	private String poder;
+
+	private int pontos;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the poder
+	 */
+	public String getPoder() {
+		return poder;
+	}
+
+	/**
+	 * @param poder
+	 *            the poder to set
+	 */
+	public void setPoder(String poder) {
+		this.poder = poder;
+	}
+
+	/**
+	 * @return the pontos
+	 */
+	public int getPontos() {
+		return pontos;
+	}
+
+	/**
+	 * @param pontos
+	 *            the pontos to set
+	 */
+	public void setPontos(int pontos) {
+		this.pontos = pontos;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public Double getValor() {
+		return valor;
+	}
+
+	/**
+	 * @param valor
+	 *            the valor to set
+	 */
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+}
