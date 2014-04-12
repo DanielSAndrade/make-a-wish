25be5810aa21582b258708a8ecc9275f09411a61
brunocf <bruno.croys@gmail.com>
2014-04-12 13:46:13 -0300

    Classe Recompensa

---
+++ src/main/java/com/ciandt/hackathon/resources/RecompensaServlet.java
@@ -0,0 +1,22 @@
+package com.ciandt.hackathon.resources;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.inject.Singleton;
+
+@SuppressWarnings("serial")
+@Singleton
+public class RecompensaServlet extends HttpServlet {
+	
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		
+		
+		req.getRequestDispatcher("/recompensa/index.jsp").forward(req, resp);
+	}
+}
